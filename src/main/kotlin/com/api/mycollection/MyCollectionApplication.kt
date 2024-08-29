786e3d9004bf7e87ae535ab2898f84bdd012bae3
GreatPark96 <park_jh6444@naver.com>
2024-08-29 10:16:23 +0900

    initial commit

---
+++ src/main/kotlin/com/api/mycollection/MyCollectionApplication.kt
@@ -0,0 +1,11 @@
+package com.api.mycollection
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyCollectionApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyCollectionApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/mycollection/MyCollectionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.mycollection
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyCollectionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
